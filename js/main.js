5bd2b00617e586cb7bcf28e1b3a03fa4eaa54d4e
Nathalia Tan <nathalia.tan@deloitte.com>
2019-05-02 12:40:38 +1000

    [FEAT] Added fade in/slide in tween

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 const controller = new ScrollMagic.Controller();
 
+let fadeInSlideIn = new TweenMax.to('#phone', 1.5, {
+    transform: 'translate(-200px, 0)',
+    opacity: '1'
+});
+
+new ScrollMagic.Scene({
+    triggerElement: "#phone",
+    reverse: false
+})
+.setTween(fadeInSlideIn)
+.addTo(controller);
+
 new ScrollMagic.Scene({
     triggerElement: "#phone-anchor"
 })
 .setPin('#phone')
-.addIndicators()
 .addTo(controller);
